5ba56ed6c158963f56e5a1a88c0b4003dcec161c
DAVI ELIZIARIO FERRANTE DE OLIVEIRA <davi.efoliveira@senacsp.edu.br>
2026-06-10 15:22:08 -0300

    atividade

--- Projeto/__init__.py
+++ Projeto/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'd6895985dce431fbae9591d52b03cfee91c072d79ad64fa33162382c5bca4f85'
+
+
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'homepage'
 
 
-from Projeto import routes
+
+from Projeto import routes , models
--- Projeto/models.py
+++ Projeto/models.py
@@ -1,12 +1,17 @@
-from Projeto import database
+from Projeto import database , login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
-class Tarefas(database.Model):
+class Tarefas(database.Model, UserMixin):
     id = database.Column(database.Integer, primary_key=True)
     nomeTarefa = database.Column(database.String)
     id_admin = database.Column(database.Integer, database.ForeignKey('usuario.id'))
     id_usuario = database.Column(database.Integer, database.ForeignKey('usuario.id'))
     funcao = database.Column(database.String, nullable=False)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Usuario.query.get(int(user_id))
 
 
 class Usuario(database.Model):
